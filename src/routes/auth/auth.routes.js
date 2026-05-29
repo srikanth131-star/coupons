17ebdc43e524632d1e34becca3f8dfeff9b361e7
srikanth131-star <couponsfeast25@gmail.com>
2026-05-29 12:44:09 +0530

    updated the changes

--- src/routes/auth/auth.routes.js
+++ src/routes/auth/auth.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { login, verify } from '../../controllers/authController.js';
+import { login, verify, refresh, logout } from '../../controllers/authController.js';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.get('/verify', verify);
+router.post('/refresh', refresh);
+router.post('/logout', logout);
 
 export default router;
